9135bdc712aa771d4355650b375d33db197897db
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2023-09-17 10:02:46 +0530

    Add files via upload

---
+++ Basic C++ Questions/even-odd.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+      int n;
+      cin >> n;
+
+      //Even
+      if(n%2 == 0)
+      {
+         cout << "The number is even" << endl;
+      }
+
+      //Odd
+      if(n%2 != 0)
+      {
+         cout << "The number is odd" << endl;
+      }
+    }
+
+    return 0;
+}
