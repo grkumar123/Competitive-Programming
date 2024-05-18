f12f9cddd41dee0ce77a57fa539156efed5f22b5
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2024-05-19 01:17:50 +0530

    Add files via upload

---
+++ Linked List/ReverseLL-Iteratively.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node *ReverseLL(Node *head)
+{
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while(temp != NULL)
+    {
+        Node *front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        Node *head = takeInput();
+        cout << "Linked List : ", print(head);
+
+        head = ReverseLL(head);
+        cout << "Linked List : ", print(head);
+    }
+
+    return 0;
+}
