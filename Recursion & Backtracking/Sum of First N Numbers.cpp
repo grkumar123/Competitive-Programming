d2fed3ba3fbd6fb8b771cddc73b0eab6b2765a39
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2024-05-21 12:25:53 +0530

    Add files via upload

---
+++ Recursion & Backtracking/Sum of First N Numbers.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int func(int n)
+{
+    // Base Case
+    if(n == 0)
+    {
+        return 0;
+    }
+
+    return n + func(n-1);
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int x = func(n);
+        cout << "Sum of N numbers : " << x;
+    }
+
+    return 0;
+}
