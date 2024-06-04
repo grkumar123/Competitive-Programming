fb86840cf2ca7904dd4b33a0ef8721dc123c01cb
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2024-06-04 18:42:16 +0530

    Add files via upload

---
+++ Linked List/Reverse a LL - Recursively.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node *ReverseLinkedList(Node *head)
+{
+    if(head == NULL || head->next == NULL) 
+    {
+        return head;
+    }
+
+    Node *newNode = ReverseLinkedList(head->next);
+
+    Node *front = head->next;
+
+    front->next = head;
+
+    head->next = NULL;
+
+    return newNode;
+
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        Node *head = takeInput();
+        print(head);
+        
+        cout << endl;
+
+        head = ReverseLinkedList(head);
+        print(head);
+    }
+
+    return 0;
+}
