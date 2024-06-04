a061116e4d8af12c987aa5cbeda03bc6f73a456c
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2024-06-04 17:12:14 +0530

    Add files via upload

---
+++ Linked List/Delete Node in LL - Recursively.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node *DeleteNode(Node *head, int i)
+{
+   if(head == NULL)
+   {
+        return head;
+   }
+
+   if(i == 0)
+   {
+        head = head->next;
+        return head;
+   }
+
+   head->next = DeleteNode(head->next, i-1);
+
+   return head;   
+   
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        Node *head = takeInput();
+        print(head);
+        cout << endl;
+        
+        int i = 5;
+
+        head = DeleteNode(head, i);
+        print(head);
+    }
+
+    return 0;
+}
