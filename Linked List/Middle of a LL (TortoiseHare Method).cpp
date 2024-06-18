b06d63c4183e086de3cdbb229534b7c396e8b603
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2024-06-18 14:53:49 +0530

    Add files via upload

---
+++ Linked List/Middle of a LL (TortoiseHare Method).cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int MiddleOfLL(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow->data;
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        Node *head = takeInput();
+        print(head);
+        
+        cout << endl;
+
+        int x = MiddleOfLL(head);
+
+        cout << x << endl;
+        
+    }
+
+    return 0;
+}
