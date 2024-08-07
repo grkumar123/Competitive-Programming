4e32417a158fbc18044069514b4e26b2a1393a91
Gyan Ranjan Kumar <103508999+grkumar123@users.noreply.github.com>
2024-08-07 20:54:06 +0530

    Add files via upload

---
+++ Linked List/Linked List - Dynamic.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// Create a Linked List Dynamically
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void print(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        // Dynamically
+        Node *n1 = new Node(5);
+        Node *head = n1;
+
+        Node *n2 = new Node(6);
+        n1->next = n2;
+
+        Node *n3 = new Node(7);
+        n2->next = n3;
+
+        print(head);
+    }
+    
+    return 0;
+}
