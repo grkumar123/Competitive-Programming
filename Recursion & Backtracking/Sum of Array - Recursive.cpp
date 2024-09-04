d95d949b4630786d29989950c3d42511785a0bbf
Gyan Ranjan Kumar <103508999+gyan2976@users.noreply.github.com>
2024-09-04 19:38:04 +0530

    Add files via upload

---
+++ Recursion & Backtracking/Sum of Array - Recursive.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int func(int *arr, int n)
+{
+    
+    int total = arr[0];
+
+    // Base Case
+    if(n == 0)
+    {
+        return 0;
+    }
+
+    int smallAns = func(arr+1, n-1);
+    return total + smallAns;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        cout << func(arr, n);
+    }
+
+    return 0;
+}
