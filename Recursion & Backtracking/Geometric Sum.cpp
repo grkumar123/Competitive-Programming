00062e9c452fde09da25071e05cddb5647a5ff1e
Gyan Ranjan Kumar <103508999+gyan2976@users.noreply.github.com>
2024-09-12 12:19:49 +0530

    Add files via upload

---
+++ Recursion & Backtracking/Geometric Sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+double func(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+
+    return func(n-1) + 1/pow(2, n);
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << func(n);
+        
+    }
+
+    return 0;
+}
